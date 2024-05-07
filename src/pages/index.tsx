d0618c685148728c7046b60e848590fe3220137c
k-arikawa0312 <s1F102402710@iniad.org>
2024-05-08 00:16:25 +0900

    爆弾ランダム生成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,27 +22,33 @@ const Home = () => {
   const pushCount = bombMap.flat().filter((cell) => cell === 0).length; //ゲーム開始したか
   const bombCount = bombMap.flat().filter((cell) => cell !== 0).length; //爆弾何個あるか
 
-  console.log(Math.random(1, 9));
-
   const clickHandler = (x: number, y: number) => {
     const newBombMap = structuredClone(bombMap);
+    console.log(x, y);
+    console.log(pushCount);
+    if (pushCount === 81) {
+      let putBomb = 0;
+      while (putBomb < 10) {
+        console.log(89);
+        const s = Math.floor(Math.random() * 9);
+        const t = Math.floor(Math.random() * 9);
+        if (newBombMap[s][t] !== 1) {
+          newBombMap[s][t] = 1;
+          putBomb += 1;
+        }
+        console.log(Math.floor(Math.random() * 9));
+      }
+      setbombMap(newBombMap);
+    }
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {bombMap.map((row, y) =>
           row.map((color, x) => (
             <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
-              <div className={styles.icon} />
-            </div>
-          )),
-        )}
-      </div>
-      <div className={styles.frontboard}>
-        {bombMap.map((row, y) =>
-          row.map((color, x) => (
-            <div className={styles.frontcell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
-              <div className={styles.flontCell} />
+              <div className={styles.icon} style={{ background: color === 0 ? '#000' : '#fff' }} />
             </div>
           )),
         )}
